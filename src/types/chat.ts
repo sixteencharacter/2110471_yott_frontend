2d57dfed6fb1c93135be25107b5c547ff85c4822
Jirapat-K-F <jirapat.flum@gmail.com>
2025-10-23 18:02:36 +0700

    add type from backend

---
+++ src/types/chat.ts
@@ -0,0 +1,4 @@
+export interface Chat {
+    name: string
+    is_groupchat: boolean
+}
---
+++ src/types/person.ts
@@ -0,0 +1,7 @@
+export interface Person {
+    uid: number
+    email: string
+    given_name: string
+    family_name: string
+    preferred_username: string
+}
