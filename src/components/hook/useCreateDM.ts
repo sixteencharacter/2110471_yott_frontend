bb2b952cc8e535a3b55a5f9652232e8cf5e2c387
6630360621 <6630360621@student.chula.ac.th>
2025-10-25 22:37:33 +0700

    add group creation to new chat room button

--- src/components/hook/useCreateDM.ts
+++ src/components/hook/useCreateDM.ts
@@ -4,33 +4,64 @@ import { useCallback } from "react"
 import { Socket } from "socket.io-client"
 
 export function useCreateDM(
-    socket: Socket | null,
-    chatRooms: Chat[],
-    setActiveRoom: (id: number) => void,
-    currentUser: Person | null,
-    refreshChatRooms?: () => void
+  socket: Socket | null,
+  chatRooms: Chat[],
+  setActiveRoom: (id: number) => void,
+  currentUser: Person | null,
+  refreshChatRooms?: () => void
 ) {
-    return useCallback(
-        (user: Person) => {
-            if (socket) {
-                console.log("Creating DM with user:", user, "and", currentUser)
-                const data = {
-                    chat_name: "ชื่อแชท",
-                    is_groupchat: false,
-                    member_ids: [currentUser?.uid, user.uid],
-                }
-                console.log(data)
-                socket.emit("create_chat", data)
+  return useCallback(
+    (payload: any) => {
+      if (payload.type === "dm") {
+          if (socket) {
+            const data = {
+              chat_name: payload.room_name,
+              is_groupchat: false,
+              member_ids: [currentUser?.uid, payload.users[0].uid],
+            };
+            console.log(
+              "Creating chatroom with users:",
+              payload.users,
+              "and",
+              currentUser
+            );
+            console.log(data);
+            socket.emit("create_chat", data);
 
-                // Refresh chat rooms after creating DM
-                if (refreshChatRooms) {
-                    setTimeout(() => refreshChatRooms(), 1000)
-                }
-            } else {
-                throw new Error("Socket not initialized")
+            // Refresh chat rooms after creating DM
+            if (refreshChatRooms) {
+              setTimeout(() => refreshChatRooms(), 1000);
             }
-            setActiveRoom(chatRooms.length + 1)
-        },
-        [socket, chatRooms, setActiveRoom, currentUser, refreshChatRooms]
-    )
+          } else {
+            throw new Error("Socket not initialized");
+          }
+          setActiveRoom(chatRooms.length + 1);
+      } else {
+        if (socket) {
+          const data = {
+            chat_name: payload.room_name,
+            is_groupchat: true,
+            member_ids: payload.users.map((user: any) => user.uid),
+          };
+          console.log(
+            "Creating chatroom with users:",
+            payload.users,
+            "and",
+            currentUser
+          );
+          console.log(data);
+          socket.emit("create_chat", data);
+
+          // Refresh chat rooms after creating DM
+          if (refreshChatRooms) {
+            setTimeout(() => refreshChatRooms(), 1000);
+          }
+        } else {
+          throw new Error("Socket not initialized");
+        }
+        setActiveRoom(chatRooms.length + 1);
+      }
+    },
+    [socket, chatRooms, setActiveRoom, currentUser, refreshChatRooms]
+  );
 }
